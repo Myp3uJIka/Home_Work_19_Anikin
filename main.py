4b6fe3b2c4639a72bfe3581bc9fd8449a5b3dd6d
Мефодий <storage@list.ru>
2022-04-02 14:47:34 +1000

    Added new user records.

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ from flask import Flask
 from flask_restx import Api
 
 from app.config import Config
+from app.dao.model.user import User
 from app.setup_db import db
 from app.views.directors import director_ns
 from app.views.genres import genre_ns
 from app.views.movies import movie_ns
+
+
+def create_data(app, db):
+    with app.app_context():
+        db.create_all()
+
+        u1 = User(username="vasya", password="my_little_pony", role="user")
+        u2 = User(username="oleg", password="qwerty", role="user")
+        u3 = User(username="oleg", password="P@ssw0rd", role="admin")
+
+        with db.session.begin():
+            db.session.add_all([u1, u2, u3])
 
 
 def create_app(config_object):
@@ -21,6 +34,7 @@ def register_extensions(app):
     api.add_namespace(director_ns)
     api.add_namespace(genre_ns)
     api.add_namespace(movie_ns)
+    # create_data(app, db)
 
 
 app = create_app(Config())
